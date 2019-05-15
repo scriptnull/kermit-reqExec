71537c5876f7e660d5c866d3d68e4e842e43ae1a
Vishnu Bharathi <vishnubharathi04@gmail.com>
2019-05-15 14:01:05 +0530

    installs python and pip if not found

--- package/x86_64/WindowsServer_2019/package.ps1
+++ package/x86_64/WindowsServer_2019/package.ps1
@@ -1,11 +1,27 @@
 $ErrorActionPreference = "Stop"
 
-function buildReqExec
-{
+function install_prereqs() {
+  echo "Checking prerequisites"
+
+  $python_package = Get-Package python2 -provider ChocolateyGet -ErrorAction SilentlyContinue
+  if (!$python_package) {
+    echo "Installing python2"
+    choco install -y python2
+  }
+
+  $pip_package = Get-Package pip -provider ChocolateyGet -ErrorAction SilentlyContinue
+  if (!$pip_package) {
+    echo "Installing pip"
+    choco install -y pip
+  }
+}
+
+function buildReqExec() {
+  echo "building reqExec binaries"
   pip install pyinstaller==3.3
   pip install -r requirements.txt
   python -m PyInstaller --clean --hidden-import=requests -F main.py
 }
 
-echo "building reqExec binaries"
+install_prereqs
 buildReqExec
