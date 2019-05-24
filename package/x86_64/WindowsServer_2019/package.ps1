183c95314c4e11de0dd4711794e8564881c7f903
Vishnu Bharathi <vishnubharathi04@gmail.com>
2019-05-24 16:07:20 +0530

    refreshenv if new package is installed

--- package/x86_64/WindowsServer_2019/package.ps1
+++ package/x86_64/WindowsServer_2019/package.ps1
@@ -3,16 +3,24 @@ $ErrorActionPreference = "Stop"
 function install_prereqs() {
   echo "Checking prerequisites"
 
+  $installed_new_package = $false
+
   $python_package = Get-Package python2 -provider ChocolateyGet -ErrorAction SilentlyContinue
   if (!$python_package) {
     echo "Installing python2"
     choco install -y python2
+    $installed_new_package = $true
   }
 
   $pip_package = Get-Package pip -provider ChocolateyGet -ErrorAction SilentlyContinue
   if (!$pip_package) {
     echo "Installing pip"
     choco install -y pip
+    $installed_new_package = $true
+  }
+
+  if ($installed_new_package) {
+    refreshenv
   }
 }
 
